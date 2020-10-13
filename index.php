0a6f3b2c6793b26d26d942c5419c85149cb45c07
Luci437 <44019738+Luci437@users.noreply.github.com>
2020-10-13 15:19:29 +0530

    13 10

--- index.php
+++ index.php
@@ -6,19 +6,18 @@
 <div class="no-group-container">
             <!-- <img src="images/no-group.png" alt="no-group" class="no-group-img"> -->
             <div class="black-cover"></div>
-            <h1 class="welcome-note">Welcome to bloom</h1>
-            <h3 class="welcome-sub-note">You might know everything in this world, but do you know yourself?</h3>
 
             <?php 
 
                 if(isset($_GET['link'])) {
                     if($_GET['link'] == 'showJoinBox') {
                         echo '
+                            <h2 class="welcome-note">Join any group</h2>
                             <form action="includes/findGroup.inc.php" method="post" class="join-group-form">
                             <table cellspacing="10" style="width: 400px;">
                                 <tr>
                                     <td>
-                                        <label for="group-id" class="group-label">Group code</label>
+                                        <label for="group-id" class="group-label">CODE</label>
                                     </td>
                                 </tr>
                                 <tr>
@@ -27,7 +26,7 @@
                                     </td>
                                 </tr>
                                 <tr align="left">
-                                    <td>
+                                    <td style="display: grid;grid-template-columns: repeat(2,1fr);">
                                         <button name="find-group-form" class="join-button"> Join group</button>
                                         <a href="index.php" class="cancel-button"> Cancel</a>
                                     </td>
@@ -38,11 +37,12 @@
                     exit();
                     }elseif($_GET['link'] == 'showCreateBox') {
                         echo '
+                        <h2 class="welcome-note">Create your group</h2>
                         <form action="includes/createGroup.inc.php" method="post" class="join-group-form">
                         <table cellspacing="10" style="width: 400px;">
                             <tr>
                                 <td>
-                                    <label for="group-id" class="group-label">Group name</label>
+                                    <label for="group-id" class="group-label">NAME</label>
                                 </td>
                             </tr>
                             <tr>
@@ -51,7 +51,7 @@
                                 </td>
                             </tr>
                             <tr align="left">
-                                <td>
+                            <td style="display: grid;grid-template-columns: repeat(2,1fr);">
                                     <button name="create-group-form" class="join-button"> Create group</button>
                                     <a href="index.php" class="cancel-button"> Cancel</a>
                                 </td>
@@ -62,6 +62,8 @@
                     }
                 }else {
                     echo '
+                        <h1 class="welcome-note">Welcome to bloom</h1>
+                        <h3 class="welcome-sub-note">You might know everything in this world, but do you know yourself?</h3>    
                         <div class="group-button-box">
                             <a href="index.php?link=showCreateBox" class="group-button"><i class="fas fa-folder-plus pdspace"></i> Create group</a>
                             <a href="index.php?link=showJoinBox" class="group-button join-group"><i class="fas fa-plus-square pdspace"></i> Join group</a>
--- showGroup.php
+++ showGroup.php
@@ -3,7 +3,100 @@
 ?>
 
 <div class="group-detail-container">
-    <div class="group-details">
+    <div class="group-member-box">
+        
+    <!-- ** -->
+        <div class="review-box">
+            <div class="review-top">
+                <h4 class="group-username">@ Kamal</h4>
+                <h5 class="group-user-about">kamalej1234@gmail.com</h5>
+            </div>
+            <form action="" class="review-bottom">
+            <i class="fas fa-comment-alt review-icon"></i>
+                <input type="text" placeholder="Say something about me" class="review-input" require name="review">
+                <input type="hidden" name="userid" value="">
+                <button type="submit" class="review-button"><i class="fas fa-paper-plane pdspace"></i> send</button>
+            </form>
+        </div>
+
+        <div class="review-box">
+            <div class="review-top">
+                <h4 class="group-username">@ Kamal</h4>
+                <h5 class="group-user-about">kamalej1234@gmail.com</h5>
+            </div>
+            <form action="" class="review-bottom">
+            <i class="fas fa-comment-alt review-icon"></i>
+                <input type="text" placeholder="Say something about me" class="review-input" require name="review">
+                <button type="submit" class="review-button"><i class="fas fa-paper-plane pdspace"></i> send</button>
+            </form>
+        </div>
+
+        <div class="review-box">
+            <div class="review-top">
+                <h4 class="group-username">@ Kamal</h4>
+                <h5 class="group-user-about">kamalej1234@gmail.com</h5>
+            </div>
+            <form action="" class="review-bottom">
+            <i class="fas fa-comment-alt review-icon"></i>
+                <input type="text" placeholder="Say something about me" class="review-input" require name="review">
+                <input type="hidden" name="userid" value="">
+                <button type="submit" class="review-button"><i class="fas fa-paper-plane pdspace"></i> send</button>
+            </form>
+        </div>
+
+        <div class="review-box">
+            <div class="review-top">
+                <h4 class="group-username">@ Kamal</h4>
+                <h5 class="group-user-about">kamalej1234@gmail.com</h5>
+            </div>
+            <form action="" class="review-bottom">
+            <i class="fas fa-comment-alt review-icon"></i>
+                <input type="text" placeholder="Say something about me" class="review-input" require name="review">
+                <button type="submit" class="review-button"><i class="fas fa-paper-plane pdspace"></i> send</button>
+            </form>
+        </div>
+    <!-- *** -->
+
+    </div>
+    <div class="group-details-box">
+        <div>
+            <img src="images/group-image.webp" alt="group-img">
+            <div class="image-black-cover"></div>
+        </div>
+        
+
+        <?php
+
+            require "includes/dh.inc.php";
+
+            if(isset($_GET['groupId'])) {
+                $groupid = $_GET['groupId'];
+                $sql = "SELECT * FROM groups WHERE id='$groupid';";
+                $result = mysqli_query($conn, $sql);
+
+                if($row = mysqli_fetch_assoc($result)) {
+                    $uid = $row['user_id'];
+                    $sql = "SELECT name FROM users where id='$uid';";
+                    echo '
+                        <h4 class="group-code">#'.$row['group_gen_id'].'</h4>
+                        <div class="group-detail-sub">
+                        <h3 class="group-name">'.$row['group_name'].'</h3>
+                    ';
+                    if($row = mysqli_fetch_assoc(mysqli_query($conn, $sql))) {
+                        echo '
+                            <h4 class="group-hostname">@ <span>'.$row['name'].'</span></h4>
+                            </div>       
+                        ';
+                    }
+                }
+            }
+        ?>
+        <div style="display: grid;grid-template-columns: repeat(2,1fr);margin-top: 16px;">
+            <h4 class="member-sub-text">TOTAL_MEMBERS</h4>
+            <h4 class="member-sub-text">TOTAL_REVIEWS</h4>
+            <h2 class="total-members">1242</h1>
+            <h2 class="total-members">34567</h1>
+        </div>
 
     </div>
 </div>
