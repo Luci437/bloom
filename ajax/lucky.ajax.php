a2a2dbacd5ab57ae8049c7c644b4960a4fb67eea
Luci437 <44019738+Luci437@users.noreply.github.com>
2020-12-22 00:44:05 +0530

    score problem corrected

--- ajax/lucky.ajax.php
+++ ajax/lucky.ajax.php
@@ -25,25 +25,7 @@
             echo "Exchange";
         }
 
-        if($_POST['mode'] == 'winner') {
-            $himid = $_POST['himid'];
-            $gameId = $_SESSION['newGame'];
-            $sql = "SELECT max(ap.scores) as mx, ap.id FROM available_players ap, addedgame ag where ag.game_id = '$gameId' AND ag.player_id = ap.id;";
-            $result = mysqli_fetch_assoc(mysqli_query($conn, $sql));
-            $isHe = $result['id'];
-            $topScore = $result['mx'];
 
-            //this was the area that caused error
-            if($isHe != $himid) {
-                $topScore++;
-                $sql = "UPDATE available_players SET scores = '$topScore' WHERE id='$himid';";
-                mysqli_query($conn, $sql);
-            }
-
-            echo "You are the winner";
-
-
-        }
 
         if($_POST['mode'] == 'boost_10') {
             $letUserid = $_POST['gainUserId'];
